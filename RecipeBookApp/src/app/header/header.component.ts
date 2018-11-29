32bde8555a220b19bc06ccb02fbffe75e1106cb2
Dare2Code <Dare2Code@users.noreply.github.com>
2018-11-28 21:45:41 -0500

    connected routing modules to main page

--- RecipeBookApp/src/app/header/header.component.ts
+++ RecipeBookApp/src/app/header/header.component.ts
@@ -1,12 +1,7 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-  @Output() selectedPage = new EventEmitter<string>();
-  onPageSelect(selectedPage: string) {
-    this.selectedPage.emit(selectedPage);
-  }
-}
+export class HeaderComponent { }
